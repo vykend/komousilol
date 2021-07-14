c221dc103c977a8c2c506ab3467bf5a6d12154bb
vykend <vykend@gmail.com>
2021-07-14 13:30:41 +0200

    Přepracováno pro komouše

---
+++ js/data.js
@@ -0,0 +1,18 @@
+export const claims = [
+  "Tam, kde lidé třou bídu s nouzí, tam budou vždy i komunisté, aby jim z té bídy pomohli!",
+  "Jsem komunistka. A ty bys byl taky, kdybys věděl, co to znamená.",
+  "Poctivou prací ke světlým zítřkům.",
+  "Neříká se komunisti ale zasraný komunisti",
+];
+
+export const generators = [
+  { url: "https://picsum.photos/800", weight: 5 },
+  { url: "https://source.unsplash.com/800x800", weight: 5 },
+  { url: "https://source.unsplash.com/800x800/?communism", weight: 10 },
+  { url: "https://source.unsplash.com/800x800/?cat", weight: 1 },
+  { url: "https://source.unsplash.com/800x800/?puppy", weight: 1 },
+  { url: "https://source.unsplash.com/800x800/?person", weight: 10 },
+  { url: "https://source.unsplash.com/800x800/?face", weight: 10 },
+];
+
+export default claims;
--- js/index.js
+++ js/index.js
@@ -1,2 +1,75 @@
-var g=["Mladým: víte, že dokážete rozhodnout volby?","Hnutí ano prostě chce žít v minulosti bez perspektivy.","Boj s korupcí, ne udávání lidí.","Investice do sportu a kultury jsou zárukou zdravé společnosti.","Potřebujeme obnovit rovnováhu přírody!","Byty je třeba stavět, ne danit.","Nejvyšší čas zastavit zadlužování naší země!","udělejme z česka sebevědomého spojence","Místo byrokratického pekla skutečná digitalizace","Staňme se digitálním tygrem!","Veřejné finance průhledně a čistě","Dítě potřebuje náruč jedné pečující osoby!","Politik má sloužit lidem a nutit stát, aby fungoval stejně","Podpořte finančně piráty a starosty","pojďme postavit ekonomiku na nohy","2 % z rozpočtu pro armádu vybojujeme!","kultura si zaslouží 1 % z rozpočtu","na šikaně živnostníků se podílet nebudeme","skutečně digitální stát, na co máme internet?","pojďme zreformovat školství. od základů.","finance řízené podle plánu","vraťme lesům jejich zdraví","1000 korun pro každého seniora z dotací Agrofertu. Ať si pan Babiš platí předvolební dárečky ze svého.","Máme už dost nekonečných korupčních kauz. Po volbách s nimi zatočíme.","Místo jednolitých polí pestrá krajina.","je čas vyrazit pomoci na jižní moravu!"],v=[{url:"https://picsum.photos/800",weight:5},{url:"https://source.unsplash.com/800x800",weight:10},{url:"https://source.unsplash.com/800x800/?communism",weight:3},{url:"https://source.unsplash.com/800x800/?cat",weight:1},{url:"https://source.unsplash.com/800x800/?puppy",weight:1},{url:"https://source.unsplash.com/800x800/?chicken",weight:1},{url:"https://source.unsplash.com/800x800/?angry",weight:3}];var z=(t,s)=>{let a=[];for(;t.length>s;){let e=t.substring(0,s).lastIndexOf(" ");e=e<=0?s:e,a.push(t.substring(0,e));let o=t.indexOf(" ",e)+1;(o<e||o>e+s)&&(o=e),t=t.substring(o)}return a.push(t),a},k=t=>t[Math.floor(Math.random()*t.length)],y=document.getElementById("picture"),n=y.getContext("2d"),b=new FontFace("Bebas Neue","url(public/BebasNeue-Bold.ttf)"),j=async()=>{await b.load(),document.fonts.add(b)},w=v.flatMap(({url:t,weight:s})=>Array(s).fill(t)),r=async t=>{let s=()=>{let o=new Image;o.src="public/logo.png",o.addEventListener("load",()=>{n.drawImage(o,525,20);let i=document.getElementById("save");i.setAttribute("download","PirStanKampan.png"),setTimeout(()=>{i.setAttribute("href",y.toDataURL("image/png").replace("image/png","image/octet-stream"))},500)})},a=await fetch(k(w)),e=new Image;e.crossOrigin="anonymous",e.src=a.url,e.addEventListener("load",()=>{n.drawImage(e,0,0),s();let o=t!=null?t:k(g),i=z(o,20).reverse(),c=i.length<5?60:40;n.font=`${c}px 'Bebas Neue'`,i.forEach((p,d)=>{let m=30,h=685,l=15,u=l+c;n.fillStyle="#f9dc4d",n.fillRect(m,h-d*u,n.measureText(p).width+2*l,u),n.textBaseline="top",n.fillStyle="black",n.fillText(p,m+l,h+l-d*u)})})},B=document.getElementById("randomize");B.onclick=()=>r();var f=document.getElementById("customText"),I=document.getElementById("submitCustomText");I.onclick=()=>{f.value&&r(f.value)};j();r();
-//# sourceMappingURL=index.js.map
+import { claims, generators } from "./data.js";
+
+const splitText = (text, maxLineLength) => {
+  const result = [];
+  while (text.length > maxLineLength) {
+    let pos = text.substring(0, maxLineLength).lastIndexOf(" ");
+    pos = pos <= 0 ? maxLineLength : pos;
+    result.push(text.substring(0, pos));
+    let i = text.indexOf(" ", pos) + 1;
+    if (i < pos || i > pos + maxLineLength) i = pos;
+    text = text.substring(i);
+  }
+  result.push(text);
+  return result;
+};
+
+const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const canvas = document.getElementById("picture");
+const ctx = canvas.getContext("2d");
+
+const unrolledGenerators = generators.flatMap(({ url, weight }) => Array(weight).fill(url));
+
+const initImage = async (customText) => {
+  const loadLogo = () => {
+    const logo = new Image();
+    logo.src = "public/logo_kscm.png";
+    logo.addEventListener("load", () => {
+      ctx.drawImage(logo, 0, 411);
+      const linkSave = document.getElementById("save");
+      linkSave.setAttribute("download", "KomunistiKampan.png");
+      setTimeout(() => {
+        linkSave.setAttribute("href", canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+      }, 500);
+    });
+  };
+
+  const imageData = await fetch(pickRandom(unrolledGenerators));
+  const image = new Image();
+  image.crossOrigin = "anonymous";
+  image.src = imageData.url;
+  image.addEventListener("load", () => {
+    ctx.drawImage(image, 0, 0);
+    loadLogo();
+    const unsplitText = customText ?? pickRandom(claims);
+    const lines = splitText(unsplitText, 20);
+    const fontSize = lines.length < 5 ? 40 : 30;
+    ctx.font = `bold ${fontSize}px Arial`;
+    lines.forEach((line, index) => {
+      const x = 740;
+      const y = 30;
+      const padding = 15;
+      const lineHeight = padding + fontSize;
+      ctx.fillStyle = "#ffffff";
+      ctx.fillRect(x - (ctx.measureText(line).width), (y + (index * lineHeight)), ctx.measureText(line).width + 2 * padding, lineHeight);
+      ctx.textBaseline = "top";
+      ctx.fillStyle = "black";
+      ctx.fillText(line, x - (ctx.measureText(line).width) + padding, y + padding/2 + (index * lineHeight));
+    });
+  });
+};
+
+const buttonRandom = document.getElementById("randomize");
+buttonRandom.onclick = () => initImage();
+
+const inputCustom = document.getElementById("customText");
+const buttonCustom = document.getElementById("submitCustomText");
+buttonCustom.onclick = () => {
+  if (inputCustom.value) {
+    initImage(inputCustom.value);
+  }
+};
+
+//initFont();
+initImage();
